55fe264cf6d6e2dba074824cccbe25a7823423f3
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2020-04-19 23:13:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,12 +7,11 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
-sed -i 's/Openwrt/Kyara/g' package/base-files/files/bin/config_generate
+sed -i 's/Openwrt/Kyara SSR+/g' package/base-files/files/bin/config_generate
 sed -i 's/Openwrt/Kyara/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/vernesong/OpenClash package/clash
 git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+git clone https://github.com/fw876/helloworld package/SSR_Plus+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
