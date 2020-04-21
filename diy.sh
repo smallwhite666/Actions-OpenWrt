d6632ba75faf1805eb6194256b177008fe3efa3a
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2020-04-21 14:38:36 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,11 +7,12 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
-sed -i 's/Openwrt/Kyara SSR+/g' package/base-files/files/bin/config_generate
-sed -i 's/Openwrt/Kyara/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/vernesong/OpenClash package/clash
 git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 git clone https://github.com/fw876/helloworld package/SSR_Plus+
+git clone https://github.com/jerrykuku/luci-theme-argon package/argon
 ./scripts/feeds update -a
 ./scripts/feeds install -a
