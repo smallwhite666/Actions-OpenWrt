04475b269f02c3faaff2767a434d92ab650c318e
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2020-06-25 16:28:04 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/vernesong/OpenClash package/clash
 git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 git clone https://github.com/fw876/helloworld package/SSR_Plus+
 ./scripts/feeds update -a
