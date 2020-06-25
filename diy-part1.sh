64fbe41423afb08f31d0089030e91cb8cc14a206
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2020-06-25 16:48:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 git clone https://github.com/fw876/helloworld package/SSR_Plus+
