2d00805caef5eaf08cb9533823a4f7b26f0e50ce
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2020-07-28 18:21:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,5 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+git clone https://github.com/jerrykuku/lua-maxminddb.git ./package/lean/
 git clone https://github.com/jerrykuku/luci-app-vssr package/SSR_Plus+
