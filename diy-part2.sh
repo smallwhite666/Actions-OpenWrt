afd026795fc238ac99930391f1b25288d8b3b16c
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2020-07-28 19:54:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@ rm -rf ./package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon/
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/jerrykuku/lua-maxminddb.git ./package/lean/
