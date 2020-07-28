ecbfcb6a29f7a24fe60008102aaf9ca9c86919c9
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2020-07-28 20:55:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 # 删除默认argon主题，并下载新argon主题
 #rm -rf ./package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon/
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon/
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jerrykuku/lua-maxminddb.git ./package/lean/
