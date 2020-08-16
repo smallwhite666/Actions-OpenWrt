57a746920738cb63ba5eb4647f5cc908b9df6171
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2020-08-16 08:26:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,4 +22,4 @@ git clone https://github.com/kenzok8/openwrt-packages.git                       
 git clone https://github.com/kenzok8/small.git                                                                                package/small
 #git clone https://github.com/fw876/helloworld.git                                                                             package/helloworld
 #git clone https://github.com/vernesong/OpenClash.git                                                                          package/openclash
-git clone https://github.com/project-openwrt/openwrt/tree/openwrt-18.06/package/lean/luci-app-turboacc.git                    package/turboacc
+svn co https://github.com/project-openwrt/openwrt/trunk/package/lean/luci-app-turboacc package/turboacc
