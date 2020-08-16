9101e4a7cd6d22179766602d55d470acdfd44c2d
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2020-08-16 08:21:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,11 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-#git clone https://github.com/Lienol/openwrt-package.git package/openwrt-package
-#git clone https://github.com/jerrykuku/luci-app-vssr package/SSR_Plus
-#git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
-#git clone https://github.com/kenzok8/small.git package/small
-#git clone https://github.com/fw876/helloworld.git package/helloworld
-#git clone https://github.com/vernesong/OpenClash.git package/openclash
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome                                                               package/luci-app-adguardhome
+#git clone https://github.com/Lienol/openwrt-package.git                                                                       package/openwrt-package
+#git clone https://github.com/jerrykuku/luci-app-vssr                                                                          package/SSR_Plus
+git clone https://github.com/kenzok8/openwrt-packages.git                                                                     package/kenzok8
+git clone https://github.com/kenzok8/small.git                                                                                package/small
+#git clone https://github.com/fw876/helloworld.git                                                                             package/helloworld
+#git clone https://github.com/vernesong/OpenClash.git                                                                          package/openclash
+git clone https://github.com/project-openwrt/openwrt/tree/openwrt-18.06/package/lean/luci-app-turboacc.git                    package/turboacc
