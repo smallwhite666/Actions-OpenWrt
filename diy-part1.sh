20ef593e30643bd3a9ea86d7fa1dd0905d036dce
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2020-08-18 10:31:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,12 +14,13 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome                                                               package/luci-app-adguardhome
 #git clone https://github.com/Lienol/openwrt-package.git                                                                       package/openwrt-package
-#git clone https://github.com/kenzok8/openwrt-packages.git                                                                     package/kenzok8
+git clone https://github.com/kenzok8/openwrt-packages.git                                                                     package/kenzok8
 #git clone https://github.com/kenzok8/small.git                                                                                package/small
 #git clone https://github.com/fw876/helloworld.git                                                                             package/helloworld
 #git clone https://github.com/vernesong/OpenClash.git                                                                          package/openclash
-git clone https://github.com/jerrykuku/luci-app-vssr.git
-git clone https://github.com/jerrykuku/lua-maxminddb.git
+#git clone https://github.com/siropboy/mypackages                                                                               package/mypackages
+#git clone https://github.com/jerrykuku/luci-app-vssr.git
+#git clone https://github.com/jerrykuku/lua-maxminddb.git
