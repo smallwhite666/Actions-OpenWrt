e2cd84c9bf7b3f95be8b1d8b60f2830f993a8dd2
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2021-02-06 22:33:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,9 +7,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #============================================================
-# 删除默认argon主题，并下载新argon主题
-rm -rf ./package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/SmallWhite/g' package/base-files/files/bin/config_generate
