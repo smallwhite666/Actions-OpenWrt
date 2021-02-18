eed1a519c4fd1f9c99fe2a2c491328853ca0ad33
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2021-02-18 08:27:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/SmallWhite/g' package/base-files/files/bin/config_generate
 # 删除默认argon主题，并下载新argon主题
-rm -rf ./package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#rm -rf ./package/lean/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
