934218f62b3bcb704de9a9a817c1a4a4611b3595
Xiaobai666 <62383580+home1256405082@users.noreply.github.com>
2021-06-03 23:25:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment a feed source
+echo "佛祖保佑，编译必成"
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome                                                               package/luci-app-adguardhome
@@ -26,3 +14,40 @@ git clone https://github.com/fw876/helloworld.git                               
 #git clone https://github.com/xiaorouji/openwrt-passwall.git                                                                    package/passwall
 #git clone https://github.com/jerrykuku/luci-app-vssr.git
 #git clone https://github.com/jerrykuku/lua-maxminddb.git
+
+
+
+
+#                  佛祖保佑       永不宕机     永无BUG              //
+
+
+#                            _ooOoo_  
+#                           o8888888o  
+#                           88" . "88  
+#                           (| -_- |)  
+#                            O\ = /O  
+#                        ____/`---'\____  
+#                      .   ' \\| |// `.  
+#                       / \\||| : |||// \  
+#                     / _||||| -:- |||||- \  
+#                       | | \\\ - /// | |  
+#                     | \_| ''\---/'' | |  
+#                      \ .-\__ `-` ___/-. /  
+#                   ___`. .' /--.--\ `. . __  
+#                ."" '< `.___\_<|>_/___.' >'"".  
+#               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+#                 \ \ `-. \_ __\ /__ _/ .-` / /  
+#         ======`-.____`-.___\_____/___.-`____.-'======  
+#                            `=---='  
+#  
+#         .............................................  
+#                  佛祖保佑             永无BUG 
+#          佛曰:  
+#                  写字楼里写字间，写字间里程序员；  
+#                  程序人员写程序，又拿程序换酒钱。  
+#                  酒醒只在网上坐，酒醉还来网下眠；  
+#                  酒醉酒醒日复日，网上网下年复年。  
+#                  但愿老死电脑间，不愿鞠躬老板前；  
+#                  奔驰宝马贵者趣，公交自行程序员。  
+#                  别人笑我忒疯癫，我笑自己命太贱；  
+#                  不见满街漂亮妹，哪个归得程序员？
